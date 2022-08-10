a7060e2008c2b393e370f75e85b49881f7d97675
CarapaceonBear <willsmith.workmail@gmail.com>
2022-08-10 17:02:04 +0100

    not working, need to move everything in app further down chain

--- src/containers/WeatherBox/WeatherBox.jsx
+++ src/containers/WeatherBox/WeatherBox.jsx
@@ -3,16 +3,19 @@ import "./WeatherBox.scss";
 import WeatherDisplay from "../../components/WeatherDisplay/WeatherDisplay";
 
 const WeatherBox = ({ weatherData }) => {
+    
+    console.log(weatherData);
+
   return (
     <div>
-        {weatherData ? 
+        {/* {weatherData ? 
             <WeatherDisplay temperature={weatherData.0temperature} weatherCode={weatherData.0weather} />
             <WeatherDisplay temperature={weatherData.2temperature} weatherCode={weatherData.2weather} />
             <WeatherDisplay temperature={weatherData.4temperature} weatherCode={weatherData.4weather} />
             <WeatherDisplay temperature={weatherData.6temperature} weatherCode={weatherData.6weather} />
             <WeatherDisplay temperature={weatherData.8temperature} weatherCode={weatherData.8weather} />
             : <p>Data loading</p>
-        }
+        } */}
     </div>
   )
 }
